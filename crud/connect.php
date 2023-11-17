6c0361ef127748aebeb6a7b38f4b04f7b0c53727
ArjhayGozun <148737647+ArjhayGozun@users.noreply.github.com>
2023-11-17 12:05:43 +0800

    Add files via upload

---
+++ crud/connect.php
@@ -0,0 +1,11 @@
+<?php
+// Database connection configuration
+$connection = mysqli_connect('localhost', 'root', '', 'db_softeng');
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL Server" . 
+    mysql1_connect_errno();
+} else {
+    echo "Arjhay L. Gozun";
+}
+?>
